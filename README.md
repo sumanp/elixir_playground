d997f876ad22f4bc34c6f2fe6b5b8f86f77e2a3e
Suman Puri <sumanpuri55@gmail.com>
2017-09-12 13:38:28 +0530

    progress

--- README.md
+++ README.md
@@ -19,4 +19,3 @@ Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_do
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/animals](https://hexdocs.pm/animals).
 
-# elixir_playground
---
+++ mix.exs
@@ -0,0 +1,27 @@
+defmodule Animals.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :animals,
+      version: "0.1.0",
+      elixir: "~> 1.5",
+      start_permanent: Mix.env == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ex_doc, "~> 0.16.4"}
+    ]
+  end
+end
